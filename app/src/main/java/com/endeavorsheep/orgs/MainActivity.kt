b2f388a3301b1fb32a0ace5968aaa6750b8cad22
Higor Jugler <higorjugler@hotmail.com>
2023-04-29 16:00:16 -0300

    Buscando as views do layout na Activity

--- app/src/main/java/com/endeavorsheep/orgs/MainActivity.kt
+++ app/src/main/java/com/endeavorsheep/orgs/MainActivity.kt
@@ -11,6 +11,12 @@ class MainActivity : Activity() {
 
         // Configure the display of view
         setContentView(R.layout.activity_main)
+        val name = findViewById<TextView>(R.id.text_name)
+        name.text = "Fruit basket"
+        val description = findViewById<TextView>(R.id.text_description)
+        description.text = "Apple, pear and grape"
+        val price = findViewById<TextView>(R.id.text_price)
+        price.text = "$ 8.99"
     }
 
 }
