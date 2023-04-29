d424fcf6257145380083876a0b54cdefab4f3677
Higor Jugler <higorjugler@hotmail.com>
2023-04-29 10:05:37 -0300

    Adicionando Views na Activity

--- app/src/main/java/com/endeavorsheep/orgs/MainActivity.kt
+++ app/src/main/java/com/endeavorsheep/orgs/MainActivity.kt
@@ -1,7 +1,18 @@
 package com.endeavorsheep.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val view = TextView(this)
+        view.setText("Welcome!")
+
+        // Configure the display of view
+        setContentView(view)
+    }
 
 }
