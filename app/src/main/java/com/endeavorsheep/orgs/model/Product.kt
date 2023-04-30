20b5148e0d3a5a56bbe95200193042b11429a3dc
Higor Jugler <higorjugler@hotmail.com>
2023-04-30 19:31:07 -0300

    Criando o produto no formulário

--- app/src/main/java/com/endeavorsheep/orgs/model/Product.kt
+++ app/src/main/java/com/endeavorsheep/orgs/model/Product.kt
@@ -2,7 +2,7 @@ package com.endeavorsheep.orgs.model
 
 import java.math.BigDecimal
 
-class Product(
+data class Product(
     val name: String,
     val description: String,
     val price: BigDecimal
--- app/src/main/java/com/endeavorsheep/orgs/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/com/endeavorsheep/orgs/ui/activity/ProductFormActivity.kt
@@ -7,6 +7,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import com.endeavorsheep.orgs.R
+import com.endeavorsheep.orgs.model.Product
+import java.math.BigDecimal
 
 class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,9 +20,24 @@ class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
         // Action for button save
         val buttonSave = findViewById<Button>(R.id.button_save)
         buttonSave.setOnClickListener {
-            val name = findViewById<EditText>(R.id.edit_name)
-            val toName = name.text.toString()
-            Log.i("ActivityProductForm", "onCreate: $toName")
+            // Field Name
+            val fieldName = findViewById<EditText>(R.id.edit_name)
+            val name = fieldName.text.toString()
+            // Field Description
+            val fieldDescription = findViewById<EditText>(R.id.edit_description)
+            val description = fieldDescription.text.toString()
+            // Field Price
+            val fieldPrice = findViewById<EditText>(R.id.edit_price)
+            val convertPrice = fieldPrice.text.toString()
+            val price = if (convertPrice.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(convertPrice)
+            }
+            // Assemble product
+            val product = Product(name, description, price)
+
+            Log.i("ActivityProductForm", "onCreate: $product")
         }
     }
 }
