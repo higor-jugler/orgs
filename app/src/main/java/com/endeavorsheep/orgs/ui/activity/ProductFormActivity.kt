2b939328fb87e0bcf4890be8a945510a9d562c99
Higor Jugler <higorjugler@hotmail.com>
2023-04-30 19:11:33 -0300

    Configurando o listener de clique

--- app/src/main/java/com/endeavorsheep/orgs/ui/activity/ProductFormActivity.kt
+++ app/src/main/java/com/endeavorsheep/orgs/ui/activity/ProductFormActivity.kt
@@ -2,6 +2,10 @@ package com.endeavorsheep.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.endeavorsheep.orgs.R
 
 class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
@@ -11,5 +15,12 @@ class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
         // Hide support bar
         supportActionBar?.hide()
 
+        // Action for button save
+        val buttonSave = findViewById<Button>(R.id.button_save)
+        buttonSave.setOnClickListener {
+            val name = findViewById<EditText>(R.id.edit_name)
+            val toName = name.text.toString()
+            Log.i("ActivityProductForm", "onCreate: $toName")
+        }
     }
 }
